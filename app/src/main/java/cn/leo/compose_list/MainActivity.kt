3c8644c980b452dfe9fdab1d95e1dc47b6ea6a5d
beiluo <beiluo@zmengs.com>
2021-07-29 15:41:28 +0800

    [add] update compose version

--- app/src/main/java/cn/leo/compose_list/MainActivity.kt
+++ app/src/main/java/cn/leo/compose_list/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
